1bf9213769ca74db3feef0dc5fc143d5b48217be
Greg Dasney <greg.dasney@digicert.com>
2026-06-08 13:00:51 -0600

    save

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -2,13 +2,17 @@
 set -euo pipefail;
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-DOCKER_IMAGE="zmk-configurob-baked"
+DOCKER_IMAGE="zmk-configurob-baked"  
 
-echo "Building dedicated Docker image for Corne SVG keymap..."
-docker build -f Dockerfile -t zmk-all .
+docker build -f Dockerfile.firmware . -t zmkthing && docker run --rm -v ./:/firmware zmkthing cp -r firmware /firmware
 
-echo "Generating Corne keymap SVG with container..."
-docker run --rm -v "$SCRIPT_DIR/drawings:/workspace/drawings" corne-draw
 
-echo "Keymap image is in: $SCRIPT_DIR/drawings/corne.svg"
-ls -la "$SCRIPT_DIR/drawings/"
+
+# echo "Building dedicated Docker image for Corne SVG keymap..."
+# docker build -f Dockerfile -t zmk-all .
+
+# echo "Generating Corne keymap SVG with container..."
+# docker run --rm -v "$SCRIPT_DIR/drawings:/workspace/drawings" corne-draw
+
+# echo "Keymap image is in: $SCRIPT_DIR/drawings/corne.svg"
+# ls -la "$SCRIPT_DIR/drawings/"
